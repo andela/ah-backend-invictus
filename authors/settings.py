c4bd6793a7ce62a79f9d38417869f19cf684aa38
3Nakajugo <edinahmargarita@gmail.com>
2019-04-23 18:24:13 +0300

    chore(configure Database):Configure Postgresql
    - update settings with postgrsql engine
    - install psycopg2
    - update requirements.txt
    - run migrations.
    - add initial migrations
    
    [Starts #165273465]

--- authors/settings.py
+++ authors/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "author_ah"),
+        "USER": os.environ.get("DB_USER", "author"),
+        "PASSWORD": os.environ.get("DB_PASS", ""),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
