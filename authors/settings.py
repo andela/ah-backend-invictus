4abfcba620d17c679e6e0042899d343658109df3
joelmugaya <joel.mugaya@andela.com>
2019-04-25 10:16:12 +0300

    Update settings.py

--- authors/settings.py
+++ authors/settings.py
@@ -146,11 +146,11 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
 }
 
-#heroku deploy settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+# #heroku deploy settings
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
