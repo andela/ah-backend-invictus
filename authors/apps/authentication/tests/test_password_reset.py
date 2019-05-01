7c49ffa69499f58e1a3859d0ea279aa4087771b3
joelmugaya <joel.mugaya@andela.com>
2019-05-01 08:59:27 +0300

    bug (validate password on reset) a password should include numbers and alphabets and special characters
    - add a method to validate the password
    
    [Delivers #165737880]

--- authors/apps/authentication/tests/test_password_reset.py
+++ authors/apps/authentication/tests/test_password_reset.py
@@ -33,3 +33,44 @@ class UserSignUpTestCase(BaseTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['message'], 'Password reset successfull.')
+
+    def test_reset_wrong_token(self):
+        """
+        test a post new password to reset
+        """
+
+        url = reverse('password_reset_token', kwargs={'token':'ytokengoeshere'})
+        response = self.client.post(url, self.reset_new_passwords_data)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data['message'], 'Ivalid link. Regenerate a reset password token')
+
+    def test_post_wrong_new_password(self):
+        """
+        test post an invalid new password to reset
+        """
+
+        url = reverse('password_reset_token', kwargs={'token':'mytokengoeshere'})
+        response = self.client.post(url, self.reset_invalid_new_passwords_data)
+
+        error_message = {
+                    "password": [
+                        "Password should include numbers and alphabets and one special character"
+                    ]
+            }
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['errors'], error_message)
+
+    def test_post_same_password(self):
+        """
+        test post an invalid new password to reset
+        """
+
+        url = reverse('password_reset_token', kwargs={'token':'mytokengoeshere'})
+        response = self.client.post(url, self.reset_same_password_data)
+
+        error_message = "New password should be different from previous password."
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['errors'][0], error_message)
